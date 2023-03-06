cd87640d8e5734576c76f2b497bc76815c4a4195
Charry Lee <lichen0426@stu.xidian.edu.cn>
2023-03-06 19:32:24 +0800

    Add a logo to REPL

--- main.go
+++ main.go
@@ -7,6 +7,15 @@ import (
 	"os/user"
 )
 
+const logo string = `
+_________ .__                                 .____                           
+\_   ___ \|  |__ _____ ______________ ___.__. |    |   _____    ____    ____  
+/    \  \/|  |  \\__  \\_  __ \_  __ <   |  | |    |   \__  \  /    \  / ___\ 
+\     \___|   Y  \/ __ \|  | \/|  | \/\___  | |    |___ / __ \|   |  \/ /_/  >
+ \______  /___|  (____  /__|   |__|   / ____| |_______ (____  /___|  /\___  / 
+        \/     \/     \/              \/              \/    \/     \//_____/  
+`
+
 // open REPL
 func main() {
 	user, err := user.Current()
@@ -14,6 +23,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf(logo)
 	fmt.Printf("Hello %s! This is the Charry programming language!\n", user.Username)
 	fmt.Printf("Feel free to type in command!\n")
 	repl.Start(os.Stdin, os.Stdout)
