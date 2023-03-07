b84d4bedaa17852befe724722b0b72453c17d151
Charry Lee <lichen0426@stu.xidian.edu.cn>
2023-03-07 09:57:55 +0800

    Add IntegerLiteral

--- ast/ast.go
+++ ast/ast.go
@@ -51,6 +51,15 @@ func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string { return i.Value }
 
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64		// actual value
+}
+
+func (il *IntegerLiteral) expressionNode() {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string { return il.Token.Literal }
+
 // let <identifier> = <Expression>;
 type LetStatement struct {
 	Token token.Token
