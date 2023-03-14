b6450fbf0a52eda82b9cb7686a490a7fa7962e2f
Charry Lee <lichen0426@stu.xidian.edu.cn>
2023-03-14 08:54:18 +0800

    Upgrade to REPL from RELL

--- repl/repl.go
+++ repl/repl.go
@@ -2,10 +2,10 @@ package repl
 
 import (
 	"bufio"
-	"charrylang/lexer"
-	"charrylang/token"
 	"fmt"
 	"io"
+	"charrylang/parser"
+	"charrylang/lexer"
 )
 
 const PROMPT = ">>"
@@ -28,9 +28,20 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Woops! parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
