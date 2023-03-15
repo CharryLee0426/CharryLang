05989771395ee3ca3e5b4d47a258ca8c250f6436
Charry Lee <lichen0426@stu.xidian.edu.cn>
2023-03-15 22:55:51 +0800

    Add integer eval test and tool functions

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,46 @@
+package evaluator
+
+import (
+	"charrylang/lexer"
+	"charrylang/object"
+	"charrylang/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input string
+		expected int64
+	} {
+		{"8", 8},
+		{"16", 16},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+// tool test functions
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+
+	return true
+}
