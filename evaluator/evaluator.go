f3a90a49ae82c57d1e18eab8e840d4d425f15723
Charry Lee <lichen0426@stu.xidian.edu.cn>
2023-03-16 17:43:43 +0800

    Add prefix expression eval and bang operator eval

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"charrylang/object"
 )
 
+var (
+	NULL = &object.Null{}
+	TRUE = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// statements
@@ -18,6 +24,9 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return &object.Boolean{Value: node.Value}
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -32,3 +41,46 @@ func evalStatements(stmt []ast.Statement) object.Object {
 
 	return result
 }
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+// !false -> true; !true -> false
+func evalBangOperatorExpression(right object.Object) object.Object {
+	// right is switched by real value but not reference
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+// switch realRight := right.(type) {
+// case *object.Boolean:
+// 	if realRight.Value == true {
+// 		return FALSE
+// 	} else {
+// 		return TRUE
+// 	}
+// case *object.Integer:
+// 	if realRight.Value == 0 {
+// 		return TRUE
+// 	} else {
+// 		return FALSE
+// 	}
+// case *object.Null:
+// 	return TRUE
+// default:
+// 	return FALSE
+// }
