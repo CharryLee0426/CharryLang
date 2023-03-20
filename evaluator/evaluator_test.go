6654983cd06d2c4302b05e7609b23a192f340fd7
Charry Lee <lichen0426@stu.xidian.edu.cn>
2023-03-20 19:51:54 +0800

    Add error handling test

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -129,6 +129,32 @@ func TestReturnStatement(t *testing.T) {
 	}
 }
 
+func TestErrorHandling(t *testing.T) {
+	tests := []struct {
+		input string
+		expected string
+	} {
+		{"5 + true;", "type mismatch: INTEGER + BOOLEAN"},
+		{"5 + true; 5;", "type mismatch: INTEGER + BOOLEAN"},
+		{"-true;", "unknown operator: -BOOLEAN"},
+		{"true + false;", "unknown operator: BOOLEAN + BOOLEAN"},
+		{"if (10 > 1) { true + false; }", "unknown operator: BOOLEAN + BOOLEAN"},
+		{"if (10 > 1) { if (10 > 1) {return true + false;} return 1;}", "unknown operator: BOOLEAN + BOOLEAN"},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		errObj, ok := evaluated.(*object.Error)
+		if !ok {
+			t.Errorf("no error object returned. got=%T(%+v)", evaluated, evaluated)
+			continue
+		}
+		if errObj.Message != tt.expected {
+			t.Errorf("wrong error message. expected=%q, got=%q", tt.expected, errObj.Message)
+		}
+	}
+}
+
 // tool test functions
 func testEval(input string) object.Object {
 	l := lexer.New(input)
