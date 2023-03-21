7a9554d717776871c418c3afb6be0a3d4f45184e
Charry Lee <lichen0426@stu.xidian.edu.cn>
2023-03-21 11:58:37 +0800

    Add Environment object

--- object/object.go
+++ object/object.go
@@ -42,6 +42,21 @@ func (rc *ReturnType) Inspect() string { return rc.Value.Inspect() }
 type Error struct {
 	Message string
 }
-
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string { return "ERROR: " + e.Message }
+
+type Environment struct {
+	store map[string]Object
+}
+func (e *Environment) Get(name string) (Object, bool) {
+	obj, ok := e.store[name]
+	return obj, ok
+}
+func (e *Environment) Set(name string, val Object) Object {
+	e.store[name] = val
+	return val
+}
+func NewEnvironment() *Environment {
+	s := make(map[string]Object)
+	return &Environment{store: s}
+}
