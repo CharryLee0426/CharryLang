2488e8cf3c1bc6a44ddd07f3c4dc244b7f6b3fb6
Charry Lee <lichen0426@stu.xidian.edu.cn>
2023-03-31 21:10:15 +0800

    Add string literal test function

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -417,6 +417,25 @@ func TestFunctionLiteralParsing(t *testing.T) {
 	testInfixExpression(t, bodyStmt.Expression, "x", "+", "y")
 }
 
+func TestStringLiteralExpressionParsing(t *testing.T) {
+	input := `"Hello World";`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	stmt := program.Statements[0].(*ast.ExpressionStatement)
+	literal, ok := stmt.Expression.(*ast.StringLiteral)
+
+	if !ok {
+		t.Fatalf("exp not *ast.StringLiteral. got=%T", stmt.Expression)
+	}
+	if literal.Value != "Hello World" {
+		t.Errorf("literal.Value not %q. got=%q", "Hello World", literal.Value)
+	}
+}
+
 func TestFunctionParameterParsing(t *testing.T) {
 	tests := []struct {
 		input string
