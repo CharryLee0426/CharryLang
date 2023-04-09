8a89cb7191b579444cd8b4c28e21fe70b573a09e
Charry Lee <lichen0426@stu.xidian.edu.cn>
2023-04-09 18:59:52 +0800

    Add index expression node

--- ast/ast.go
+++ ast/ast.go
@@ -156,6 +156,26 @@ func (ie *InfixExpression) String() string {
 	return out.String()
 }
 
+type IndexExpression struct {
+	Token token.Token // '[' token
+	Left Expression
+	Index Expression
+}
+
+func (ie *IndexExpression) expressionNode() {}
+func (ie *IndexExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IndexExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString("[")
+	out.WriteString(ie.Index.String())
+	out.WriteString("])")
+
+	return out.String()
+}
+
 type Boolean struct {
 	Token token.Token
 	Value bool
