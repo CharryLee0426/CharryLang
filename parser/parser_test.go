ceceb10c972bda34833ee08704c07b3815f58054
Charry Lee <lichen0426@stu.xidian.edu.cn>
2023-04-09 19:00:08 +0800

    Add index expression parsing test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -370,6 +370,29 @@ func TestIfElseExpression(t *testing.T) {
 	}
 }
 
+func TestIndexExpressionParsing(t *testing.T) {
+	input := "myArray[1 + 1]"
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	indexExp, ok := stmt.Expression.(*ast.IndexExpression)
+
+	if !ok {
+		t.Fatalf("exp not *ast.IndexExpression. got=%T", stmt.Expression)
+	}
+
+	if !testIdentifier(t, indexExp.Left, "myArray") {
+		return
+	}
+
+	if !testInfixExpression(t, indexExp.Index, 1, "+", 1) {
+		return
+	}
+}
+
 func TestFunctionLiteralParsing(t *testing.T) {
 	input := `fn(x, y) { x + y; }`
 
