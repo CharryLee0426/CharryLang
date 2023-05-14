a1f48a180a8ad8979900b021074a4bf760a23372
Charry Lee <lichen0426@stu.xidian.edu.cn>
2023-05-14 21:39:26 +0800

    Add hash object and hashkey object and methods

--- object/object.go
+++ object/object.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"charrylang/ast"
 	"fmt"
+	"hash/fnv"
 	"strings"
 )
 
@@ -134,3 +135,32 @@ type Builtin struct {
 }
 func (b * Builtin) Type() ObjectType { return BUILTIN_OBJ }
 func (b * Builtin) Inspect() string { return "builtin function" }
+
+type Hash struct {
+	Pairs map[Object]Object
+}
+
+type HashKey struct {
+	Type ObjectType
+	Value uint64
+}
+
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
